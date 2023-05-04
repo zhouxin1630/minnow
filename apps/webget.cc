c10a682c9c9434d72708753f459bd4b04ccc57d6
zxwang <zhouxin1630@163.com>
2023-05-04 13:10:14 +0000

    lab0: get_URL complete

--- apps/webget.cc
+++ apps/webget.cc
@@ -9,8 +9,17 @@ using namespace std;
 
 void get_URL( const string& host, const string& path )
 {
-  cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
-  cerr << "Warning: get_URL() has not been implemented yet.\n";
+  TCPSocket socket{};
+  socket.connect(Address(host, "http"));
+  socket.write("GET " + path + " HTTP/1.1\r\nHost: " + host + "\r\nConnection: close\r\n\r\n");
+  socket.shutdown(SHUT_WR);
+  while (!socket.eof())
+  {
+    string buffer;
+    socket.read(buffer);
+    cout << buffer;
+  }
+  socket.close();
 }
 
 int main( int argc, char* argv[] )
